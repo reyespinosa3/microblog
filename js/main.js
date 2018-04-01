09c50071658bbf8df94e9636359740d8aaf9e8c3
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-31 19:39:49 -0500

    updated sytle to add new section for every new post entry

--- js/main.js
+++ js/main.js
@@ -2,22 +2,31 @@ console.log("Ready to go!");
 
 //Get new blog post into array
 
-const newPost = ["Nice site!", "Great Blog!", "Love the background!", "Show me how to do this!"];
+const newPost = ["Love the background!"];
 
 const readyToBlog = () => {
     let form = document.getElementById('microblogform');
     form.addEventListener('submit', function (e) {
       e.preventDefault();
       newPost.push(document.getElementById('microblogpost').value);
-      latestPosts();
+      // latestPosts();
+      upDateList();
       document.getElementById('microblogpost').value = "";
     });
 }
 
-const latestPosts = () => {
-  document.getElementById('first').innerHTML = newPost[newPost.length - 1];
-  document.getElementById('second').innerHTML = newPost[newPost.length -2];
-  document.getElementById('third').innerHTML = newPost[newPost.length -3];
+const upDateList = () => {
+  let post = document.getElementById('microblogpost').value;
+  let element = document.createElement("section");
+  document.getElementById('postList').append(element);
+  element.innerHTML = post;
 }
+
+
+// const latestPosts = () => {
+//   document.getElementById('first').innerHTML = newPost[newPost.length - 1];
+//   document.getElementById('second').innerHTML = newPost[newPost.length -2];
+//   document.getElementById('third').innerHTML = newPost[newPost.length -3];
+// }
 
 window.onload=readyToBlog();
